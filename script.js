597e7d90ae6cfd4f3b1c8543443dba662dd77aa2
sally100284-cyber <sally100284@gmail.com>
2026-07-13 23:39:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const b=document.getElementById('play'),a=document.getElementById('audio');b.onclick=()=>{if(a.paused){a.play();b.textContent='❚❚'}else{a.pause();b.textContent='▶'}};a.onended=()=>b.textContent='▶';

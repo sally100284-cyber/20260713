5f1d1c1788132b787cbeae47b648a7e056e6a1c6
sally100284-cyber <sally100284@gmail.com>
2026-07-14 21:18:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const playBtn = document.getElementById("play");
 const audio = document.getElementById("audio");
+
 playBtn.addEventListener("click", () => {
-  if(audio.paused){
-    audio.play();
-    playBtn.textContent = "❚❚";
-  } else {
-    audio.pause();
-    playBtn.textContent = "▶";
-  }
+
+    if(audio.paused){
+
+        audio.play();
+        playBtn.textContent = "❚❚";
+
+    }else{
+
+        audio.pause();
+        playBtn.textContent = "▶";
+
+    }
+
 });
+
 audio.addEventListener("ended", () => {
-  playBtn.textContent = "▶";
+
+    playBtn.textContent = "▶";
+
 });
